596be0f0fa28412e38fe3dc7657e7d1fdabe5da1
SuperDev <119829153+koalaworld20210104@users.noreply.github.com>
2023-03-21 21:04:39 +0200

    delete-packagelock-file

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,8 @@
-import react from "react";
 import Hero from "../components/Hero";
 import "../styles/Home.css";
-import CardList from "../components/CardList";
 import { hotDropsData } from "../constants/MockupData";
 
-
-
 const Home = () => {
-
-
   return (
     <div id="home">
       <Hero list={hotDropsData} />
